0ce5981d871d82a5534be35ad3d8ea709fbab25c
Lissa Urbano <lissaurbano@gmail.com>
2020-09-15 11:40:37 -0300

    first commit

---
+++ desafio2/Funcionario.java
@@ -0,0 +1,52 @@
+package desafio2;
+
+import jdk.jfr.Percentage;
+
+public class Funcionario extends Pessoa {
+    
+    //atributos
+    private double salarioInicial;
+    private double salarioTotal;
+    private Percentage percentual;
+
+    //construtor
+    public Funcionario(int anoContratacao, double salarioInicial, Percentage percentual) {
+    this.salarioInicial = getSalarioInicial();
+    this.salarioTotal = salarioTotal;
+    this.percentual = getPercentual();
+    }
+    
+    public double aumentoSalarial(int anoContratacao){
+
+    }
+
+    //getters e setters
+    public double getSalarioInicial() {
+        return salarioInicial;
+    }
+
+    public void setSalarioInicial(double salarioInicial) {
+        this.salarioInicial = salarioInicial;
+    }
+
+    public double getSalarioTotal() {
+        return salarioTotal;
+    }
+
+    public void setSalarioTotal(double salarioTotal) {
+        this.salarioTotal = salarioTotal;
+    }
+
+    public Percentage getPercentual() {
+        return percentual;
+    }
+
+    public void setPercentual(Percentage percentual) {
+        this.percentual = percentual;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Salario 2020 =" + this.getSalarioTotal();
+    }
+}
---
+++ desafio2/Pessoa.java
@@ -0,0 +1,30 @@
+package desafio2;
+
+public abstract class Pessoa {
+    
+    //atributos
+    private String nome;
+    private String rg;
+
+    //setters e getters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", rg=" + rg + "]";
+    }
+}
